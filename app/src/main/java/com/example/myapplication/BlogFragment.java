b8df4b0e4f45394e78d6c29ff00601fddda537a0
ZiedHrzlh <ziedharzallah66@gmail.com>
2023-11-12 11:21:39 +0100

    rounded corners+color changes

--- app/src/main/java/com/example/myapplication/BlogFragment.java
+++ app/src/main/java/com/example/myapplication/BlogFragment.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,11 +19,10 @@ public class BlogFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_blog, container, false);
 
+
+
         // TODO: Handle button click events or other logic as needed
 
         return view;
     }
 }
-
-
-
